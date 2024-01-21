9bb0febe7ccbca764290e05002317c448eb9d59f
AishwaryaSh25 <157266479+AishwaryaSh25@users.noreply.github.com>
2024-01-21 11:53:04 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//Setting up firebase with our website
+
 
 const firebaseApp = firebase.initializeApp({
   apiKey: "AIzaSyBeauu8hUfOkE1ZlxexUm-xzczmpIB2HtE",
@@ -12,13 +12,13 @@ const firebaseApp = firebase.initializeApp({
 const db = firebaseApp.firestore();
 const auth = firebaseApp.auth();
 
-//Sign up function
+
 const signUp = () => {
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
   console.log(email, password);
 
-  //firebase code
+
 
   firebase
     .auth()
@@ -33,11 +33,11 @@ const signUp = () => {
     });
 };
 
-//Sign in function
+
 const signIn = () => {
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
-  //firebase code
+  
   firebase
     .auth()
     .signInWithEmailAndPassword(email, password)
@@ -51,14 +51,14 @@ const signIn = () => {
     });
 };
 function searchFunction() {
-  // Get input value and convert to lowercase for case-insensitive search
+  
   var input, filter, ul, li, a, i, txtValue;
   input = document.getElementById('searchInput');
   filter = input.value.toLowerCase();
   ul = document.getElementById('resultsList');
   li = ul.getElementsByTagName('li');
 
-  // Loop through all list items, and hide those that don't match the search query
+  
   for (i = 0; i < li.length; i++) {
       a = li[i];
       txtValue = a.textContent || a.innerText;
